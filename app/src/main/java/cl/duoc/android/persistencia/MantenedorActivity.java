5872aec59075ba9d0dc938b7904857720881f88e
Santiago Neira <sant.neira@profesor.duoc.cl>
2016-10-22 23:58:58 -0300

    App de TODOs con SQLite + GridView y Adapter con Cursor.

--- app/src/main/java/cl/duoc/android/persistencia/MantenedorActivity.java
+++ app/src/main/java/cl/duoc/android/persistencia/MantenedorActivity.java
@@ -1,12 +1,21 @@
 package cl.duoc.android.persistencia;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.SeekBar;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +24,63 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import static cl.duoc.android.persistencia.ToDoDbHelper.*;
+
 public class MantenedorActivity extends AppCompatActivity {
+
+    private GridView gridViewTareas;
+    private CursorAdapter adapter;
+    private String[] projection = {
+            TAREA_ID
+            , TAREA_TEXTO
+            , TAREA_PRIORIDAD
+            , TAREA_FECHA_CREACION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mantenedor);
 
+        configurarUsuario();
+        manejarEventoSeekBarPrioridad();
+        configurarAdapterGridView();
+
+        gridViewTareas.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MantenedorActivity.this, "ID:"+id+"||position:"+position, Toast.LENGTH_LONG ).show();
+                ToDoDbHelper toDoDbHelper = new ToDoDbHelper(MantenedorActivity.this);
+                SQLiteDatabase writableDb = toDoDbHelper.getWritableDatabase();
+                writableDb.delete(TAREA_TABLE, "_id = ?", new String[]{id+""});
+                adapter.swapCursor(getCursor(projection));
+                return true;
+            }
+        });
+    }
+
+    private void manejarEventoSeekBarPrioridad() {
+        SeekBar sbPrioridad = (SeekBar) findViewById(R.id.sbPrioridad);
+        sbPrioridad.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progreso, boolean fromUser) {
+                TextView tvPrioridad = (TextView) MantenedorActivity.this.findViewById(R.id.tvPrioridad);
+                tvPrioridad.setText("Prioridad: "+progreso);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void configurarUsuario() {
         SharedPreferences sp = getSharedPreferences(getString(R.string.archivoPreferencias), Context.MODE_PRIVATE);
         String usuario = sp.getString("usuario", "");
         Log.v("Usuario", "usuario: " + usuario);
@@ -32,6 +91,63 @@ public class MantenedorActivity extends AppCompatActivity {
         } else {
             tvConectadoComo.setText("Bienvenido usuario");
         }
+    }
+
+    private void configurarAdapterGridView() {
+        int[] to = {
+                R.id.tvTareaId
+                , R.id.tvTarea
+                , R.id.tvPrioridad
+                , R.id.tvFechaCreacion
+        };
+
+        Cursor cursor = getCursor(projection);
+
+        gridViewTareas = (GridView) findViewById(R.id.gridViewTareas);
+        adapter = new SimpleCursorAdapter(this, R.layout.tarea_item, cursor, projection, to, 0);
+        gridViewTareas.setAdapter(adapter);
+    }
+
+    private Cursor getCursor(String[] projection) {
+        ToDoDbHelper toDoDbHelper = new ToDoDbHelper(this);
+        SQLiteDatabase readableDb = toDoDbHelper.getReadableDatabase();
+        Cursor cursor = readableDb.query(TAREA_TABLE, projection, null, null, null, null, TAREA_PRIORIDAD + " DESC");
+        return cursor;
+    }
+
+    private String textoFromViewId(int id) {
+        EditText et = (EditText) findViewById(id);
+        String s = et.getText().toString();
+        return s;
+    }
+
+    public void guardarTarea(View view) {
+        String tarea = textoFromViewId(R.id.etTarea);
+        SeekBar sbPrioridad = (SeekBar) findViewById(R.id.sbPrioridad);
+        int prioridad = sbPrioridad.getProgress();
+        ToDoDbHelper toDoDbHelper = new ToDoDbHelper(this);
+        SQLiteDatabase db = toDoDbHelper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(TAREA_TEXTO, tarea);
+        cv.put(TAREA_PRIORIDAD, prioridad);
+        db.insert(TAREA_TABLE, null, cv);
+
+        // actualiza el cursor del adapter para que se
+        // vean reflejados los cambios
+        adapter.swapCursor(getCursor(projection));
+
+        // notifica la creación con un TOAST
+        Toast.makeText(this, "Tarea guardada", Toast.LENGTH_LONG).show();
+    }
+
+    // Muestra la versión instalada de SQLite en el dispositivo
+    public void versionSQLite(View view) {
+        Cursor cursor = SQLiteDatabase.openOrCreateDatabase(":memory:", null).rawQuery("select sqlite_version() AS sqlite_version", null);
+        String sqliteVersion = "";
+        while(cursor.moveToNext()){
+            sqliteVersion += cursor.getString(0);
+        }
+        Toast.makeText(this, sqliteVersion, Toast.LENGTH_LONG).show();
     }
 
     /* Checks if external storage is available for read and write */
